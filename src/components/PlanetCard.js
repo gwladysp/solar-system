489d10235b22c40f7bd77998e5d1688ce9b75b3f
Gwladys <gwladys.pinel54@gmail.com>
2021-11-10 03:46:24 +0100

    Correction : planet names

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,12 +3,11 @@ import {Link, useRouteMatch} from "react-router-dom";
 function PlanetCard(props) {
     let match = useRouteMatch();
     let {planet} = props;
-
     return <>
         <div>
-            <h3>{planet.name}</h3>
+            <h3>{planet.id.charAt(0).toUpperCase() + planet.id.slice(1)}</h3>
             <p>Short planet description</p>
-            <Link to={`${match.url}/${planet.englishName}`} planet={props}>Discover</Link>
+            <Link to={`${match.url}/${planet.id}`} planet={props}>Discover</Link>
         </div>
     </>
 }
