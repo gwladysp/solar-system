3561794aab7318aaab2211a6025652979fe1cd5b
Gwladys <gwladys.pinel54@gmail.com>
2021-11-28 00:00:04 +0100

    add tailwindcss custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          light: '#262544',
+          DEFAULT: '#254594',
+          dark: '#070625'
+        },
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
