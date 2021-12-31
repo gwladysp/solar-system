c9f91098b2f98ecad00a851538127024bf94bd29
Gwladys <gwladys.pinel54@gmail.com>
2022-01-01 00:48:27 +0100

    add planets images

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,14 +3,17 @@ import { Link, useRouteMatch } from 'react-router-dom';
 function PlanetCard(props) {
   let match = useRouteMatch();
   let { planet } = props;
+
+  let planetName = planet.englishName.replace(/[0-9]/g, '').trim();
+
   return (
     <>
       <div key={planet.id} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
         <div className="flex-shrink-0">
           <img
             className="h-48 w-2/3 object-cover object-center"
-            src={'/images/' + planet.englishName + '.svg'}
-            alt={planet.englishName}
+            src={'/images/' + planetName + '.svg'}
+            alt={planetName}
           />
         </div>
         <div className="flex-1 bg-blue-light p-6 flex flex-col justify-between">
@@ -18,7 +21,7 @@ function PlanetCard(props) {
             <p className="text-sm font-medium text-indigo-600">Main planets</p>
             <div>
               <p className="text-xl font-semibold text-gray-200">
-                {planet.englishName.charAt(0).toUpperCase() + planet.englishName.slice(1)}
+                {planetName.charAt(0).toUpperCase() + planetName.slice(1)}
               </p>
               <p className="mt-3 text-base text-gray-500">
                 Short planet description of approximatly two or three lines giving basics
@@ -29,7 +32,7 @@ function PlanetCard(props) {
               type="button"
               className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-6"
             >
-              <Link to={`${match.url}/${planet.englishName.toLowerCase()}`} planet={props}>
+              <Link to={`${match.url}/${planetName}`} planet={props}>
                 Discover
               </Link>
             </button>
