c22c2b77ec5d6cfb331702ea20a4de4ec82b0833
Gwladys <58272493+gwladysp@users.noreply.github.com>
2022-01-03 15:37:06 +0100

    feat(loading): add loading animation

--- components/animations/Loading.jsx
+++ components/animations/Loading.jsx
@@ -1,12 +1,41 @@
+import { SOLAR_SIZE_IN_EM } from "../../src/constants";
+
 function Loading() {
   return (
     <>
-      <div className="mx-auto max-w-sm text-center pt-16">
-        <img
-          className="mx-auto"
-          src="https://c.tenor.com/FBeNVFjn-EkAAAAC/ben-redblock-loading.gif"
-          alt="loading"
+      <div id="galaxy" className="mt-16 max-w-lg mx-auto no-3d">
+        <div
+          id="sun"
+          style={{
+            fontSize: `${SOLAR_SIZE_IN_EM}vw`,
+            backgroundImage: "url(/images/sun.svg)",
+          }}
+          className="no-3d"
         />
+        <div
+          className="orbit"
+          style={{
+            width: `15vw`,
+            height: `15vw`,
+            marginTop: `-7.5vw`,
+            marginLeft: `-7.5vw`,
+            animationDuration: `5s`,
+            border: `none`,
+          }}
+        >
+          <div className="pos" style={{ animationDuration: `5s` }}>
+            <div
+              className="orbitingPlanet"
+              style={{
+                fontSize: `3vw`,
+                backgroundImage: `url(images/earth.svg)`,
+                animationDuration: `8s`,
+                transform: `rotate3d(1, 0, 0, 90deg)`,
+              }}
+            />
+          </div>
+        </div>
+        <span className="absolute left-[44%] bottom-[-2vw]">Loading...</span>
       </div>
     </>
   );
