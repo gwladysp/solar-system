a6e0580125cc6df421115c0200a08a44ed248657
Corentin <gwladys.pinel54@gmail.com>
2022-01-13 03:20:47 +0100

    feat(sun): update data

--- data/the-sun.js
+++ data/the-sun.js
@@ -3,6 +3,29 @@ export const sun = {
   "id": "sun",
   "name": "Sun",
   "moons": null,
+  "description":"The Sun is a star. It is a huge, spinning, glowing sphere of hot gas. The Sun is just like the stars that you see in the night sky. It appears so much larger and brighter than the other stars because we are so close to it. The Sun is the center of our Solar System and contains most of the mass in the Solar System. All of the planets in our Solar System, including Earth, orbit around the Sun.",
+  "composition": [
+    {
+      "element": "hydrogen",
+      "percentage": 73
+    },
+    {
+      "element": "helium",
+      "percentage": 25
+    },
+    {
+      "element": "oxygen",
+      "percentage": 0.8
+    },
+    {
+      "element": "carbon",
+      "percentage": 0.3
+    },
+    {
+      "element": "others",
+      "percentage": 0.9
+    }
+  ],
   "semimajorAxis": 0,
   "perihelion": 0,
   "aphelion": 0,
