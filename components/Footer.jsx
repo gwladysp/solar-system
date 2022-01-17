02a35f5c3177bb804288665adf9c3fd450f276a2
Corentin <gwladys.pinel54@gmail.com>
2022-01-17 01:24:24 +0100

    feat(footer): add

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <>
+            <div className="mt-24 border-t-2 w-full border-blue py-4 px-8 flex justify-end items-center">
+                <a href="https://github.com/gwladysp/solar-system" className="cursor-pointer" target="_blank">
+                    <img src="/images/icon-github.png" alt="Github mark" className="opacity-25"/>
+                </a>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,14 +2,16 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import "../styles/index.css";
 import "../styles/planetAnimations.css";
+import Footer from "../components/Footer"
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Navbar/>
+            <Component {...pageProps} />
+            <Footer/>
+        </>
+    );
 }
 
 export default MyApp;
