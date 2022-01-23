80a9bd7ac298af8a9f938a1ec4ca0b82177424a3
Corentin <gwladys.pinel54@gmail.com>
2022-01-23 23:22:22 +0100

    feat(footer): update to fit in homepage

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,11 @@
+import {useRouter} from "next/router";
+
 function Footer() {
+    // footer has no background in home page
+    const router = useRouter();
     return (
         <>
-            <div className="mt-24 border-t-2 w-full border-blue py-4 px-8 flex justify-end items-center">
+            <div className={`${router.pathname !== "/" ? 'mt-24 w-full' : 'absolute bottom-0 border-none right-0'} border-t-2 border-blue py-4 px-8 flex justify-end items-center`}>
                 <a href="https://github.com/gwladysp/solar-system" className="cursor-pointer" target="_blank">
                     <img src="/images/icon-github.png" alt="Github mark" className="opacity-25"/>
                 </a>
